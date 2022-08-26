6c2e65a0e45dd6e21d5fc894541e843e4744dac3
weiglszonja <wszonja@gmail.com>
2022-08-26 12:45:18 +0200

    add tests for AcousticWaveformWidget

--- src/pynwb/tests/test_acoustic_waveform_series.py
+++ src/pynwb/tests/test_acoustic_waveform_series.py
@@ -1,8 +1,10 @@
 import numpy as np
+from nwbwidgets.controllers import StartAndDurationController
 
 from pynwb.testing import TestCase
 
 from ndx_sound import AcousticWaveformSeries
+from ndx_sound.widgets import AcousticWaveformWidget
 
 
 class TestAcousticWaveformSeriesConstructor(TestCase):
@@ -28,3 +30,26 @@ class TestAcousticWaveformSeriesConstructor(TestCase):
             np.testing.assert_array_equal(acoustic_waveform_series.data, data)
             self.assertEqual(acoustic_waveform_series.rate, 42000.)
             self.assertEqual(acoustic_waveform_series.unit, "n.a.")
+
+    def test_AcousticWaveformWidget_without_time_window_controller(self):
+        data = np.random.randint(1000, size=(10000,))
+        acoustic_waveform_series = AcousticWaveformSeries(
+            name="acoustic_stimulus",
+            data=data,
+            rate=42000.,
+            description="acoustic_stimulus",
+        )
+
+        AcousticWaveformWidget(acoustic_waveform_series)
+
+    def test_AcousticWaveformWidget_with_time_window_controller(self):
+        data = np.random.randint(1000, size=(10000,))
+        acoustic_waveform_series = AcousticWaveformSeries(
+            name="acoustic_stimulus",
+            data=data,
+            rate=42000.,
+            description="acoustic_stimulus",
+        )
+
+        controller = StartAndDurationController(tmin=0.1, tmax=0.3)
+        AcousticWaveformWidget(acoustic_waveform_series, controller)
